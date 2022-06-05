f3bfeb876f45a852c67b020ee21ca4e7d9ad4f2b
MohamedAshraf <55748253+Op262@users.noreply.github.com>
2022-06-05 05:58:35 +0200

    commit backend

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const limiter = require('express-rate-limit');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT;
+const { db_uri } = require('./config.json')
+
+
+mongoose
+  .connect(db_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log(`database connected`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+require('./routes')(app)
+app.get('/', (req, res) => {
+  res.send(200);
+  res.end();
+});
+app.listen(PORT, () => console.log(`backend on port ${PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,16 +19,5 @@ function App() {
     </div>
   );
 }
-function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="about">About</Link>
-      </nav>
-    </div>
-  );
-}
 
 export default App;
